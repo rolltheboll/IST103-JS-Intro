79165307e4787decc07e9b74d552e52b0fd26bf4
”Rolltheboll <ralphserranoastete@gmail.com>
2024-04-12 04:35:31 -0700

    committ

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
-alert("Hello World")
+//alert("Hello World")'
+//console.log("helloworld")
+let identification = "Mike";
+let personage ="43";
+let personmarried ="true";
+const ishealive ="yes";
+console.log ("random person name is" +  identification)
+console.log("his age is" +  personage)
+console.log("is he married" +  personmarried)
